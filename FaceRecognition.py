43a8a870bfbffd375d6ed108a3648f60706245f6
Yuvraj Bal <baly@mcmaster.ca>
2023-11-28 00:43:11 -0500

    Added pid

--- FaceRecognition.py
+++ FaceRecognition.py
@@ -1,4 +1,9 @@
 import cv2
+import numpy as np
+w,h = 360,240
+fbRange =[6200,6800]
+pid=[0.4,0.4,0]
+pError=0
 
 def FindFace(img):
     # Load the pre-trained Haar Cascade face detector
@@ -25,14 +30,37 @@ def FindFace(img):
     else:
         return img, [[0,0],0]
 
+def trackFace( info, w, pid, pError):
+    area = info[1]
+    x,y = info[0]
+    fb=0
+    error= x - w//2
+    speed=pid[0]*error + pid[1]*(error-pError)
+    speed = int(np.clip(speed,-100,100))
 
+    if area > fbRange[0] and area < fbRange[1]:
+        fb=0
+    if area > fbRange[1]:
+        fb=-20
+    elif area < fbRange[0] and area != 0:
+        fb=20
+    if x==0:
+        speed=0
+        error=0
+
+
+    print(speed,fb)
+    #me.send_rc_control(0,fb,0,speed)
+    return error
 
 # Initialize the camera
 cap = cv2.VideoCapture(0)  # 0 corresponds to the default camera (you can change it to 1, 2, etc., if you have multiple cameras)
 while True:
     ret, frame = cap.read()
+    frame=cv2.resize(frame,(w,h))
     frames,info=FindFace(frame)
-    print('center',info[0],'area',info[1] )
+    pError= trackFace(info,w,pid,pError)
+    #print('center',info[0],'area',info[1] )
     cv2.imshow('Face Detection', frames)
     cv2.waitKey(2)
 
